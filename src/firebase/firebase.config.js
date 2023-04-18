33a945d8dc2bf15aae28526e5865fd46754e23b9
Shamim raza <shamimraza411@gmail.com>
2023-04-19 01:05:21 +0600

    header

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyApc2FkEqnm5MSzja6w44bp3YnrNXWme48",
+  authDomain: "fir-react-tailwind-c3063.firebaseapp.com",
+  projectId: "fir-react-tailwind-c3063",
+  storageBucket: "fir-react-tailwind-c3063.appspot.com",
+  messagingSenderId: "116289174758",
+  appId: "1:116289174758:web:7b3d918ffd9823a0650f27"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ src/main.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from './components/Layout/Main';
+import Login from './components/Login';
+import Register from './components/Register';
+import Home from './components/Home';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children:[
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: '/login',
+        element: <Login></Login>
+      },
+      {
+        path: '/register',
+        element: <Register></Register>
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+      <RouterProvider router={router} />
+  </React.StrictMode>,
+)
